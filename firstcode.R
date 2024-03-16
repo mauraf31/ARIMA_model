7b60e475ae6cedd3ca93a08d05a826394e25b575
JackPo1126 <163676312+JackPo1126@users.noreply.github.com>
2024-03-16 15:36:28 -0500

    Update firstcode.R

--- firstcode.R
+++ firstcode.R
@@ -28,6 +28,12 @@ plot(appl_ts)
 acf(appl_close)
 pacf(appl_close)
 
-#obvious upward trend and slow decreasing acf. strong evidence of nonstationarity. Need to Transform
-appl_close2 = transform(appl_close, AAPL.Close = log(AAPL.Close))
-tsplot(appl_close2, main="Apple Stock", pch = 20)
+
+#obvious upward trend and slow decreasing acf. strong evidence of nonstationarity. Need to Transform the data
+appl_close2 = transform(appl_close, diff_log_AAPL_Close = diff(log(AAPL.Close), lag = 1))
+plot(appl_close2$diff_log_AAPL_Close, type = "l", main = "First Order Difference of Log-transformed Apple Stock")
+acf(appl_close2$diff_log_AAPL_Close)
+pacf(appl_close2$diff_log_AAPL_Close)
+
+#Acf cuts off and the data looks way better than it did before. Now we can move onto the model smoothing and selection
+
