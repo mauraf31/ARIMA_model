7f57a6798a842c54cdb7d7fd3ec47c21928325ac
JackPo1126 <163676312+JackPo1126@users.noreply.github.com>
2024-03-16 15:09:19 -0500

    Update firstcode.R

--- firstcode.R
+++ firstcode.R
@@ -20,6 +20,14 @@ library(TSA)
 library(astsa)
 plot(y=appl_close$AAPL.Close,x = appl_close$Date, main="Apple Stock",grid())
 
+#Check Stationarity
 appl_ts = ts(data=appl_data$AAPL.Close,frequency=12)
 class(appl_ts)
 plot(appl_ts)
+
+acf(appl_close)
+pacf(appl_close)
+
+#obvious upward trend and slow decreasing acf. strong evidence of nonstationarity. Need to Transform
+appl_close2 = transform(appl_close, AAPL.Close = log(AAPL.Close))
+tsplot(appl_close2, main="Apple Stock", pch = 20)
