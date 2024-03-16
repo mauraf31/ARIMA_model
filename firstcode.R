c788d91428cfe42cecfa3496d80b8ae9db8f4086
Maura Flanagan <mauraf31@tamu.edu>
2024-03-16 13:07:20 -0500

    loading in the dataset

--- firstcode.R
+++ firstcode.R
@@ -1,2 +1,15 @@
 install.packages("tidyverse")
 library(tidyverse)
+
+install.packages("quantmod")
+library(quantmod)
+
+getSymbols('AAPL',src = 'yahoo')
+
+install.packages("data.table")
+library(data.table)
+
+aapl_data=as.data.table(AAPL)
+names(aapl_data)[0] = c('Date')
+
+aapl_data['date'] = as.Date(aapl_data$date, "%d-%b-%y")
