e34459cfe9715c4adbd20444e70974e27355dcc8
Maura Flanagan <mauraf31@tamu.edu>
2024-03-16 14:14:21 -0500

    more code

--- firstcode.R
+++ firstcode.R
@@ -8,5 +8,15 @@ getSymbols('AAPL',src = 'yahoo')
 head(AAPL)
 dim(AAPL)
 
-AAPL_clean=na.omit(AAPL)
-dim(AAPL_clean)
+library(data.table)
+appl_data = as.data.table(AAPL)
+names(appl_data)[1] = c("Date")
+head(appl_data)
+
+appl_close = appl_data[,c(1,5)]
+
+
+library(TSA)
+library(astsa)
+tsplot(appl_close, main="Apple Stock", type="o", pch=19, ylab="")
+
