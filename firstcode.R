c853a1521fcf100b46f34824f3865f3504d62bd5
Maura Flanagan <mauraf31@tamu.edu>
2024-03-16 13:15:12 -0500

    found there were no missing data values

--- firstcode.R
+++ firstcode.R
@@ -5,11 +5,8 @@ install.packages("quantmod")
 library(quantmod)
 
 getSymbols('AAPL',src = 'yahoo')
+head(AAPL)
+dim(AAPL)
 
-install.packages("data.table")
-library(data.table)
-
-aapl_data=as.data.table(AAPL)
-names(aapl_data)[0] = c('Date')
-
-aapl_data['date'] = as.Date(aapl_data$date, "%d-%b-%y")
+AAPL_clean=na.omit(AAPL)
+dim(AAPL_clean)
